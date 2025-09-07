65833fca2bd2e38f92ab1d90d7515b4e1f87b8e5
edensulfurea <12345678+gretabruno91@gmail.com.noreply.github.com>
2025-09-07 19:51:05 +0200

    feat: render iniziale + log click celle

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-const Gameboard = (()=>{
-    let gameboard = ['','','','','','','','',''];
-    const render = () =>{
-        let boardHTML = '';
-        gameboard.forEach((square, index)=>{
-            boardHTML += `<div class="square" id=square-${index}">${square}</div>`
+let board= [null,null,null,null,null,null,null,null,null];
+let currentPlayer= "X";
+let gameOver= false;
+let winner= null;
+let winningLine = null;
 
-        })
+const statusEl = document.getElementById("status");
+const boardEl = document.getElementById("board");
+const cells = Array.from(boardEl.querySelectorAll(".cell"));
+const restartBtn = document.getElementById("restart");
+
+
+
+function render(){
+    cells.forEach((cell)=>{
+        const idx= Number(cell.dataset.index);
+        const val= board[idx];
+
+        cell.textContent = val? val: "";
+        cell.classList.remove("win");
+
+           if (Array.isArray(winningLine) && winningLine.includes(idx)) {
+      cell.classList.add("win");
     }
-    document.querySelector("#gameboard").innerHTML = boardHTML;
-    return{
-        render,
+  });
+
+        if(gameOver){
+            statusEl.textContent = winner? `Ha vinto ${winner}!` : 'Pareggio!';
+        }else{
+            statusEl.textContent = `Tocca a ${currentPlayer}`;
+        }
+
     }
-
-}) ();
-
+    render();
 
 
+cells.forEach((cell)=>{
+    cell.addEventListener('click', ()=>{
+        const idx= Number(cell.dataset.index);
+        console.log("Hai cliccato la cella:", idx);
+    });
+});
 
-const startButton = document.querySelector("#start-button");
-startButton.addEventListener("click", ()=>{
-    //Game.start();
-})
 
 
